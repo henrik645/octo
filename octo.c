a297fdac0f21a011ce79a153b7f9d60c3794302d
Henrik Vester <henrik645@gmail.com>
2015-05-09 16:09:14 +0200

    Deleted unnecessary code at 'i'

--- octo.c
+++ octo.c
@@ -422,51 +422,6 @@ int main(int argc, char *argv[]) {
                         }
                         break;
                     case 'i':
-                        // linesInputted = 0;
-                        // inputSize = 0;
-                        // while (1) { //Clears newline from stdin
-                            // c = getchar();
-                            // if (c != '\n') {
-                                // ungetc(c, stdin); //Puts the character back
-                                // break;
-                            // }
-                        // }
-                        // while (1) {
-                            // fgets(inputLine, SCREEN_WIDTH, stdin);
-                            // if (strcmp(inputLine, ".\n") == 0) {
-                                // break;
-                            // } else if (strcmp(inputLine, "\n") == 0) {
-                                // inputLine[0] = '\0';
-                            // } else {
-                                // strtok(inputLine, "\n");
-                            // }
-                            // if (linesInputted + 1 > inputSize) {
-                                // inputSize = linesInputted + 1;
-                                // newInput = realloc(input, inputSize * SCREEN_WIDTH * sizeof(char));
-                                // if (newInput == NULL) {
-                                    // printf("Error: out of memory\n");
-                                    // exit(1);
-                                // }
-                                // input = newInput;
-                            // }
-                            // strcpy(input + (linesInputted * SCREEN_WIDTH * sizeof(char)), inputLine);
-                            // linesInputted++;
-                        // }
-                        // lines += linesInputted; //Adds to the buffer
-                        // newBuffer = realloc(buffer, (lines + 1) * SCREEN_WIDTH * sizeof(char)); //Adds to the buffer
-                        // if (newBuffer == NULL) {
-                            // fprintf(stderr, "Error: out of memory");
-                            // free(buffer);
-                            // exit(2);
-                        // }
-                        // buffer = newBuffer;
-                        // memmove(buffer + ((line + linesInputted) * SCREEN_WIDTH), buffer + (line * SCREEN_WIDTH), (lines - line - linesInputted) * SCREEN_WIDTH * sizeof(char)); //Shifts the memory up x spaces (the number of lines entered)
-                        // for (x = 0; x < linesInputted; x++) {
-                            // strcpy(buffer + ((line + x) * SCREEN_WIDTH), (input + (x * SCREEN_WIDTH * sizeof(char))));
-                        // }
-                        // free(input); //Resets the input
-                        // input = NULL;
-                        // unsaved = 1;
                         insert_lines(line);
                         break;
                     case 'd':
